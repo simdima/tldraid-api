52564e6e5b03b11ea87efc48f74b50861fe803da
Dima Sim <89066354+simdima@users.noreply.github.com>
2024-03-23 08:59:52 +0200

    Add current version info under root endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LanguagesModule } from './languages/languages.module';
 import { UtilitiesModule } from './utilities/utilities.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), LanguagesModule, UtilitiesModule],
+  providers: [AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
